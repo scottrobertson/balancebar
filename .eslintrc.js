30201b6e333b5ddb1a83e238f2a31445d92d9d1c
Scott Robertson <scottymeuk@gmail.com>
2021-02-06 13:32:51 +0000

    Adding prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,26 @@ module.exports = {
   root: true,
   parser: "vue-eslint-parser",
   parserOptions: {
-    sourceType: 'module'
+    sourceType: "module",
   },
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard',
-    'plugin:vue/base',
-    'plugin:vue/recommended',
-  ],
+  extends: ["plugin:vue/recommended", "plugin:prettier-vue/recommended", "prettier/vue"],
   globals: {
-    __static: true
+    __static: true,
   },
   plugins: [
     // 'html'
   ],
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    "arrow-parens": 0,
     // allow async-await
-    'generator-star-spacing': 0,
+    "generator-star-spacing": 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "prettier-vue/prettier": ["warn"],
+  },
+};
