46a1385751292cdb05f8aa665a32e7400d1380f5
Scott Robertson <scottymeuk@gmail.com>
2021-02-06 13:20:13 +0000

    Show when consent expires

--- src/renderer/components/connections.vue
+++ src/renderer/components/connections.vue
@@ -29,12 +29,13 @@
               <p class="text-sm font-medium text-gray-900 dark:text-white">
                 {{ credential.provider.display_name }}
               </p>
-              <p
+              <div
                 v-if="credential.consent_created_at"
                 class="text-xs dark:text-white"
               >
-                Connected {{ credential.consent_created_at | moment('from') }}
-              </p>
+                <div>Connected {{ credential.consent_created_at | moment('from') }}</div>
+                <div>Expires {{ credential.consent_expires_at | moment('from') }}</div>
+              </div>
               <a
                 class="underline cursor-pointer text-xs text-gray-500 dark:text-gray-300"
                 @click="disconnectCredential(credential)"
