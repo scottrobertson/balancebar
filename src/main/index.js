88cce7d526d7aa6b261f16a017c90156e3e8a5fd
Scott Robertson <scottymeuk@gmail.com>
2021-02-09 21:06:31 +0000

    Enabling electron-updater

--- src/main/index.js
+++ src/main/index.js
@@ -103,22 +103,13 @@ app.on("ready", () => {
   });
 });
 
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
+import { autoUpdater } from "electron-updater";
 
-/*
-import { autoUpdater } from 'electron-updater'
+autoUpdater.on("update-downloaded", () => {
+  autoUpdater.quitAndInstall();
+});
 
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+app.on("ready", () => {
+  // if (process.env.NODE_ENV === "production") autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdates();
+});
