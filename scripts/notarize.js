a74677cc420593db06688ee0c6606a8e4b4ee324
Scott Robertson <scottymeuk@gmail.com>
2021-02-17 13:07:26 +0000

    Only notarize if apple id is present

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -2,17 +2,21 @@ require("dotenv").config();
 const { notarize } = require("electron-notarize");
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== "darwin") {
-    return;
+  if (process.env.APPLE_ID) {
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== "darwin") {
+      return;
+    }
+
+    const appName = context.packager.appInfo.productFilename;
+
+    return await notarize({
+      appBundleId: "com.scottrobertson.balancebar",
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASS,
+    });
+  } else {
+    console.log("Skipping notarize step");
   }
-
-  const appName = context.packager.appInfo.productFilename;
-
-  return await notarize({
-    appBundleId: "com.scottrobertson.balancebar",
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
-  });
 };
