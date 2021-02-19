0ee9ceeda1d9741175c05cf8871bde1dad2e2de7
Scott Robertson <scottymeuk@gmail.com>
2021-02-19 14:05:22 +0000

    Use refresh icon

--- src/renderer/components/home-header.vue
+++ src/renderer/components/home-header.vue
@@ -5,15 +5,25 @@
         <div class="ml-4 mt-2">
           <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-white pt-2">Balances</h3>
 
-          <div v-if="credentials">
-            <div v-if="lastRefreshed && accounts">
-              <span class="text-gray-400 text-xs">Last updated {{ lastRefreshed }} - <a class="underline cursor-pointer" @click="refreshAccounts">Refresh</a></span>
-            </div>
+          <template v-if="credentials">
+            <template v-if="lastRefreshed && accounts">
+              <div class="text-gray-400 text-xs flex">
+                <div>Updated {{ lastRefreshed }}</div>
+                <svg class="cursor-pointer w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @click="refreshAccounts">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  ></path>
+                </svg>
+              </div>
+            </template>
 
-            <div v-else>
+            <template v-else>
               <span class="text-gray-400 text-xs">Updating...</span>
-            </div>
-          </div>
+            </template>
+          </template>
         </div>
         <div class="ml-4 mt-2 flex-shrink-0 pt-2">
           <button
